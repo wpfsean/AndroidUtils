acb0c308410e6ba0e29b609737e64a0edaccaf2f
wpfsean <wpfsean@126.com>
2018-09-13 16:56:58 +0800

    Initial commit

---
+++ myutils/src/main/java/com/zhketech/client/myutils/LogUtils.java
@@ -0,0 +1,18 @@
+package com.zhketech.client.myutils;
+
+import android.util.Log;
+
+/**
+ * Created by Root on 2018/9/13.
+ */
+
+public class LogUtils {
+
+    public LogUtils(){
+    }
+
+    public static void log(String tag){
+        Log.i("TAG",tag);
+    }
+
+}
